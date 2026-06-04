588a4c0f9beb2ac2aeda0d356af90c4d57f52610
Ze4llaboizeng <228241144+Ze4llaboizeng@users.noreply.github.com>
2026-06-04 19:13:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import { extension_settings, saveSettingsDebounced } from "../../../extensions.js";
+import { extension_settings, getContext } 
+const { saveSettingsDebounced } = getContext();
 import { eventSource, event_types as eventTypes }    from "../../../../script.js";
 const EXT_NAME = "SillyTavern-Performance-Boost";
 const EXT_PATH = `scripts/extensions/third-party/${EXT_NAME}`;
